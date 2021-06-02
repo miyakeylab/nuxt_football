555eebac37c41a949fdbc713a8570d63139fc806
miyakeylab <miyasaviola@gmail.com>
2021-06-02 12:10:19 +0900

    first

---
+++ football/test/e2e/pageObjects/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  commands: [],
+  url: 'http://localhost:3002',
+  elements: {
+    docButton: {
+      selector: '.button--green'
+    },
+    githubButton: {
+      selector: '.button--grey'
+    }
+  }
+}
